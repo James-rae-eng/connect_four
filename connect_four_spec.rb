c8b04eeffc28afa8129d611736501173162f7516
James Rae <jrae151515@yahoo.co.uk>
2022-07-20 19:59:22 +0100

    game mostly completed, learning experience for rspec so updates not given regularly, only few features left to implement

--- connect_four_spec.rb
+++ connect_four_spec.rb
@@ -2,7 +2,6 @@ require_relative 'script'
 
 
 describe Game do
-    #no test needed for welcome_message
 
     describe "#player_name" do
         subject(:name_input) {described_class.new} 
@@ -10,18 +9,142 @@ describe Game do
         it "displays the correct player number, 1 in this case" do
             message = "Please enter player 1's name."
             expect(name_input).to receive(:puts).with(message).once
-            name_input.player_name(1)
+            name_input.player_name(1, "\u{25CD}")
         end
+    end
 
-        before do
-            player_input = "name"
-            allow(name_input).to receive(:gets).and_return(player_input)
+    describe '#play' do
+        context 'Governing class to run the flow of the game' do 
+            #subject(:play_game) { described_class.new}
+            
+        end
+    end
+
+    describe '#valid_selection?' do
+        context 'make sure only user column input of single numbers 1-7 returns true' do 
+            subject(:valid) { described_class.new}
+            
+            it "returns true when given 2" do
+                expect(valid.valid_selection?(2)).to eq(true)
+            end
+
+            it "returns false when given 0" do
+                expect(valid.valid_selection?(0)).to eq(false)
+            end
+
+            it "returns false when given 8" do
+                expect(valid.valid_selection?(8)).to eq(false)
+            end
+
+            it "returns false when given punctuation (!), after being converted to integer" do
+                punc = "!"
+                expect(valid.valid_selection?(punc.to_i)).to eq(false)
+            end
         end
     end
 
 
+end
+
+describe Board do
+
+    describe "#valid_move?" do
+        context "using column 2 as a selection example" do
+            subject(:valid) {described_class.new} 
+            let(:slots) {valid.instance_variable_get(:@slots)}
+
+            it "returns true when column is empty" do
+                expect(valid.valid_move?(2)).to eq(true)
+            end
+
+            it "returns true when column has only 2 filled spaces" do
+                slots[0][1] = "fill"
+                slots[1][1] = "fill"
+                expect(valid.valid_move?(2)).to eq(true)
+            end
+
+            it "returns false when column has top space filled" do
+                slots[5][1] = "fill"
+                expect(valid.valid_move?(2)).to eq(false)
+            end
+        end
+    end
+
+    describe "#board_full?" do
+        subject(:board) {described_class.new} 
+        let(:slots) {board.instance_variable_get(:@slots)}
+
+        it "returns false when column is empty" do
+            expect(board.board_full?).to eq(false)
+        end
+
+        it "returns true when top row is filled (by virtue all lower ones already filled)" do
+            slots[5][0] = "fill"
+            slots[5][1] = "fill"
+            slots[5][2] = "fill"
+            slots[5][3] = "fill"
+            slots[5][4] = "fill"
+            slots[5][5] = "fill"
+            slots[5][6] = "fill"
+            expect(board.board_full?).to eq(true)
+        end
+    end
+
+    describe "#row_win" do
+        subject(:board) {described_class.new} 
+        let(:slots) {board.instance_variable_get(:@slots)}
+        symbol = "x"
+
+        it "returns false when row is empty" do
+            expect(board.row_win(symbol)).to eq(false)
+        end
+
+        it "returns false when first row has 4 symbols but not consecutive" do
+            slots[0][0] = symbol
+            slots[0][1] = symbol
+            slots[0][3] = symbol
+            slots[0][4] = symbol
+            expect(board.row_win(symbol)).to eq(false)
+        end
 
 
+        it "returns true when second row is filled with 4 in a row" do
+            slots[0][0] = symbol
+            slots[0][1] = symbol
+            slots[0][2] = symbol
+            slots[0][3] = symbol
+            expect(board.row_win(symbol)).to eq(true)
+        end
+    end
 
+    describe "#column_win" do
+        subject(:board) {described_class.new} 
+        let(:slots) {board.instance_variable_get(:@slots)}
+        symbol = "x"
+
+        it "returns false when row is empty" do
+            expect(board.column_win(symbol)).to eq(false)
+        end
+
+        it "returns false when first row has 4 symbols but not consecutive" do
+            slots[0][0] = symbol
+            slots[1][0] = symbol
+            slots[4][0] = symbol
+            slots[5][0] = symbol
+            expect(board.column_win(symbol)).to eq(false)
+        end
+
+
+        it "returns true when second row is filled with 4 in a row" do
+            slots[0][1] = symbol
+            slots[1][1] = symbol
+            slots[2][1] = symbol
+            slots[3][1] = symbol
+            expect(board.column_win(symbol)).to eq(true)
+        end
+    end
+
+
+    
 
 end
--- script.rb
+++ script.rb
@@ -8,7 +8,70 @@ class Player
     end
 end
 
+class Board
+    def initialize()
+        @slots = Array.new(6) {Array.new(7,"\u{25EF}")}
+    end
+
+    def display_board
+        joined_row = @slots.map {|nest| nest.join(" ")}
+        joined_row.unshift(["1 2 3 4 5 6 7"])
+        puts joined_row.reverse
+    end
+
+    def add_counter(selection, current_player)
+        done = 0
+        @slots.map do |row|
+            if row[(selection-1)] == "\u{25EF}" && done == 0
+                row[(selection-1)] = current_player.symbol
+                done += 1
+            end
+        end
+    end
+
+    def valid_move?(selection)
+        @slots[5][(selection-1)] != "\u{25EF}" ? false : true
+    end
+
+    def board_full?
+        @slots[5].include?("\u{25EF}") ? false : true
+    end
+
+    def row_win(symbol)
+        @slots.each do |row|
+            row.each_cons(4) do |four_slots|
+                return true if four_slots.all? {|slot| slot == symbol}
+            end
+        end
+        return false
+    end
+
+    def column_win(symbol)
+        0.upto(5) do |i|
+            column = [@slots[0][i], @slots[1][i], @slots[2][i], @slots[3][i], @slots[4][i], @slots[5][i]]
+            column.each_cons(4) do |four_slots|
+                return true if four_slots.all? {|slot| slot == symbol}
+            end
+        end
+        return false
+    end
+
+    def right_dia_win(symbol)
+        #complete this
+        return false
+    end
+
+    def left_dia_win(symbol)
+        #complete this
+        return false
+    end
+end
+
 class Game
+    def initialize (current_player = nil)
+        @current_player = current_player
+        @board = Board.new
+    end
 
     def welcome_message
         puts "\nLet's play Connect four!
@@ -22,16 +85,87 @@ class Game
     end
 
 
-    def player_name(number)
+    def player_name(number, symbol)
         puts "Please enter player #{number}'s name."
         name = gets.chomp
-        Player.new(name, "o")
+        Player.new(name, symbol)
     end
 
-    
+    def player_set_up
+        symbol_1 = "\u{25CD}"
+        symbol_2 = "\u{25CF}"
+        @player_1 = player_name(1, symbol_1)
+        @player_2 = player_name(2, symbol_2)
+        @current_player = @player_1
+    end
 
+    def switch_player
+        if @current_player == @player_1
+            @current_player = @player_2
+        else
+            @current_player = @player_1
+        end
+    end
 
+    def valid_selection?(selection)
+        if selection.to_s.length >= 2 
+            false
+        elsif selection == 8 || selection == 9 || selection == 0
+            false
+        else 
+            true
+        end     
+    end
+
+    def player_turn
+        puts "#{@current_player.name} please type in a column number to add your counter."
+        while selection = gets.chomp.to_i
+            case
+            when valid_selection?(selection) == false || @board.valid_move?(selection) == false
+                puts "invalid selection, please choose again"
+            else
+                break
+            end
+        end
+        @board.add_counter(selection, @current_player)
+        @board.display_board
+    end
+
+    def check_winner
+        s = @current_player.symbol
+        if @board.row_win(s) == true || @board.column_win(s) == true || @board.right_dia_win(s) == true || @board.left_dia_win(s) == true
+            true
+        else
+            false
+        end
+    end
+
+    def game_over?
+        if @board.board_full? == true
+            true
+        elsif check_winner == true
+            true
+        else 
+            false
+        end
+    end
+
+    def display_winner
+        puts "\n#{@current_player.name} is the winner!"
+    end
+
+    def play
+        welcome_message
+        player_set_up
+        puts "Here is an example board:"
+        @board.display_board
+        until game_over? == true do
+            switch_player
+            player_turn
+        end
+        display_winner
+    end
 end
 
-#game = Connect_four.new
-#game.welcome_message
+game = Game.new
+game.play
